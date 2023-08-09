8ecd1e7ec10ea9a30f45d5a28fbc7d7b39954643
Tharak-N <tharak.n@innovatily.com>
2023-08-09 12:58:50 +0530

    short cut

--- routes/router.js
+++ routes/router.js
@@ -5,20 +5,19 @@ const authorization = require('../middlewares/authoriztion');
 const login = require('../middlewares/login')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router
+.get('/', (req, res) => {
     res.send("Hola!")
 })
-
-router.post('/register', (req, res) => {
+.post('/register', (req, res) => {
     res.status(201).send("Registration successfull")
 })
-
-router.post('/login', login())
-router.use(authorization())
-router.get('/home', (req, res) => {
+.post('/login', login())
+.use(authorization())
+.get('/home', (req, res) => {
     res.send("Welcome to Home")
 })
-router.use('/about-me', about)
+.use('/about-me', about)
 
 module.exports = router;
 
