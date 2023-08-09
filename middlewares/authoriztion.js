fbe17aabae1c024302b61e3a640499ce4b98c737
Tharak-N <tharak.n@innovatily.com>
2023-08-09 13:59:24 +0530

    updated for token retrieve

--- middlewares/authoriztion.js
+++ middlewares/authoriztion.js
@@ -2,7 +2,7 @@
 const auth = require('../utilities/token')
 
 const authorization = () => (req, res, next) => {
-    let token = req.headers['authorization'];
+    let token = req.headers['authorization'].split('Bearer ')[1];
     let isVerified = auth.tokenVerify(token)
     if(!!isVerified) next()
     else res.status(401).end("Token Expired")
